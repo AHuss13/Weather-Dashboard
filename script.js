bc0eb9236dd23e8fa52b490a9ce0c27386c31180
Adam Huss <adamhuss13@gmail.com>
2024-06-03 16:27:12 -0500

    added wind and humidity information

--- script.js
+++ script.js
@@ -10,27 +10,27 @@ const currentWind = document.getElementById("currentwind");
 
 const currentHumidity = document.getElementById("currenthumidity");
 
-const apikey = "843fa40ad68a96668befb0da86d9b44b"
+const apikey = "98f3c7db6cc18d9ceb403e078c661de5";
 
 function citySearch() {
   // Look for city
-  const city= cityInput.value
-  console.log(city)
+  const city = cityInput.value;
+  console.log(city);
 
-  const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=imperial`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=imperial`;
   fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    currentCity.textContent = data.name
-    currentTemp.textContent = `Temp: ${data.main.temp}`
-  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      currentCity.textContent = data.name;
+
+      currentTemp.textContent = `Temp: ${data.main.temp}`;
+      currentWind.textContent = `Wind: ${data.wind.speed}`;
+      currentHumidity.textContent = `Humidity: ${data.main.humidity}`;
+    });
 }
 
-searchBtn.addEventListener("click", citySearch)
-
-
-
+searchBtn.addEventListener("click", citySearch);
 
 // 5 day forecast
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
